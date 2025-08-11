fb2e032c3996018d7461e034f42d342cbef5cfd6
Vineet Mali <7038vineet@gmail.com>
2025-08-11 21:49:06 +0530

    commit

---
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NewArrivals = () => {
+  const NewArrivals = [
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price:120,
+      images: {
+
+      }
+
+    },
+  ];
+  return <div>NewArrivals</div>;
+};
+
+export default NewArrivals;
