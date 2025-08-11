b76f1b4a770d44a4274dc43c0b74576dca805c75
Vineet Mali <7038vineet@gmail.com>
2025-08-11 21:53:53 +0530

    commit

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -5,11 +5,74 @@ const NewArrivals = () => {
     {
       _id: 1,
       name: "Stylish Jacket",
-      price:120,
+      price: 120,
       images: {
-
-      }
-
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
+    },
+    {
+      _id: 1,
+      name: "Stylish Jacket",
+      price: 120,
+      images: {
+        url: "https://picsum.photos/500/500",
+        altText: "Stylish Jacket",
+      },
     },
   ];
   return <div>NewArrivals</div>;
