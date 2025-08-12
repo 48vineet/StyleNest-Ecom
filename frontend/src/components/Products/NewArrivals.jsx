c3ee69b223d10de53a5f2406b38f5e9b08dd51d9
Vineet Mali <7038vineet@gmail.com>
2025-08-12 18:52:35 +0530

    changed

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 const NewArrivals = () => {
   const NewArrivals = [
     {
@@ -7,7 +7,7 @@ const NewArrivals = () => {
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=1",
         altText: "Stylish Jacket",
       },
     },
@@ -16,66 +16,98 @@ const NewArrivals = () => {
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=2",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 3,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=3",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 4,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=4",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 5,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=9",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 6,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=6",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 7,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=7",
         altText: "Stylish Jacket",
       },
     },
     {
-      _id: 1,
+      _id: 8,
       name: "Stylish Jacket",
       price: 120,
       images: {
-        url: "https://picsum.photos/500/500",
+        url: "https://picsum.photos/500/500?random=8",
         altText: "Stylish Jacket",
       },
     },
   ];
-  return <div>NewArrivals</div>;
+  return (
+    <section>
+      <div className="container mx-auto text-center mb-10 relative">
+        <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
+        <p className="text-lg text-gray-600 mb-8">
+          Discover the latest styles straight off the runway, freshly added to
+          keep your wardrobe on the cutting edge of fashion.
+        </p>
+        {/* Scroll Button */}
+        <div className="absolute right-0 bottom-[-30px] flex space-x-2">
+          <button className="p-2 rounded-full border border-black bg-white text-black">
+            <FiChevronLeft className="text-2xl" />
+          </button>
+          <button className="p-2 rounded-full border border-black bg-white text-black">
+            <FiChevronRight className="text-2xl" />
+          </button>
+        </div>
+      </div>
+      {/* Scrollable Content */}
+      <div className="container mx-auto overflow-x-scroll flex space-x-6 relative">
+        {NewArrivals.map((product) => {
+          return (
+            <div key={product._id}>
+              <img
+                src={product.images.url}
+                alt={product.images.altText || product.name}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default NewArrivals;
