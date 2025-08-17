6101c66bc121ba6c3c0fff5e234240b6a8dd379a
Vineet Mali <7038vineet@gmail.com>
2025-08-17 19:25:44 +0530

    add login page and route to application

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home></Home>} />
+          <Route path="login" element={<Login></Login>}></Route>
         </Route>
         <Route> {/*Admin layout*/}</Route>
       </Routes>
