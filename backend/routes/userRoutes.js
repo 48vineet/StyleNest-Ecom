54d6f67b15f46df8015cbbd18d6b869c8cbec2a6
Vineet Mali <7038vineet@gmail.com>
2025-09-17 11:52:47 -0700

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-const router = express.Router();
+const router = express.Router(); 
 
 //? @route POST /api/user/register
 
